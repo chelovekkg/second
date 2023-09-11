d4015ff2e3d7832546b83cd700264de34b814fd7
Timur Bikbulatov <timur.bikb@gmail.com>
2023-09-11 21:10:31 +0500

    Уменьшили размер формы и цвет на зеленый

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(279, 164);
+            this.button1.Location = new System.Drawing.Point(141, 167);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(204, 56);
             this.button1.TabIndex = 0;
@@ -45,8 +45,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(503, 434);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
