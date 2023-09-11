ddacd15068ac8dff437622dc9c2c96a07d2e1951
Timur Bikbulatov <timur.bikb@gmail.com>
2023-09-11 21:15:03 +0500

    удалили кнопку

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,19 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(125, 175);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(204, 56);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Закрыть";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -48,7 +36,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(477, 455);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
             this.ResumeLayout(false);
@@ -56,8 +43,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
